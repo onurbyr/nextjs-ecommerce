ab4f3fc46786b114bde90d2f48d592c3acf82c18
onurbyr <byronurr@gmail.com>
2024-01-31 17:44:01 +0300

    Product Details 2

--- src/app/productDetails/[productId]/page.tsx
+++ src/app/productDetails/[productId]/page.tsx
@@ -5,7 +5,9 @@ import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import { getProduct } from "@/services";
 import { ProductProps } from "@/types";
-import { LoadingIndicator } from "@/components";
+import { AnimationButton, LoadingIndicator, StarRating } from "@/components";
+import { colors } from "@/constants";
+import { addToCart } from "@/redux/features/cartSlice";
 
 const ProductDetails = ({ params }: { params: { productId: string } }) => {
   const dispatch = useAppDispatch();
@@ -47,9 +49,37 @@ const ProductDetails = ({ params }: { params: { productId: string } }) => {
         height={0}
         sizes="100vw"
         style={{ width: "100%", height: "200px", marginBottom: "0.5rem" }}
-        className="object-contain"
+        className="object-contain flex md:flex-1"
       />
-      <div>{product.description}</div>
+      <div className="flex flex-col flex-1 gap-2">
+        <div className="text-xl font-semibold font-mono">{product.title}</div>
+        <div className="flex">
+          <StarRating
+            rate={product.rating.rate}
+            color={colors.mainRed}
+            size="0.8rem"
+          />
+          <div className="text-[#8D99AE] text-xs ml-1">
+            {product.rating.count}
+          </div>
+        </div>
+        <div className="text-main-red text-2xl">
+          ${product.price.toFixed(2)}
+        </div>
+        <div>{product.description}</div>
+        <div className="flex">
+          <div className="mr-1 text-secondary-text">Category:</div>
+          <div>{product.category}</div>
+        </div>
+        <div className="flex my-2">
+          <AnimationButton
+            buttonTitle="ADD TO CART"
+            onClick={() => {
+              dispatch(addToCart(product));
+            }}
+          ></AnimationButton>
+        </div>
+      </div>
     </main>
   );
 };
